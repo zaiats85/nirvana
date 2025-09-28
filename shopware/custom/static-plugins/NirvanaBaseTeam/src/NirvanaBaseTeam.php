583ea9c675a8892e939cbe7cfca8b601c86e7412
Lyndos <makslundos@gmail.com>
2025-09-28 12:51:18 +0300

    feat: Add nirvana theme
    Refs: feature/DPL-19

---
+++ shopware/custom/static-plugins/NirvanaBaseTeam/src/NirvanaBaseTeam.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace NirvanaBaseTeam;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class NirvanaBaseTeam extends Plugin implements ThemeInterface
+{
+}
